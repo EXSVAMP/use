9489315f9e3dd70b3edaa2e7d1d156e6ee331d81
jiangyuanyuan <jiangyuanyuan@1b2b.cn>
2016-09-18 11:43:58 +0800

    iothtml2 header

--- modules/main/main.js
+++ modules/main/main.js
@@ -23,6 +23,30 @@ app.service("baseUrl",function(){
         }
     }
 })
-app.controller("MasterCtrl",function($scope){
+app.controller("MasterCtrl",function($scope, $cookieStore, $http, baseUrl){
+	$scope.header_com_mask = false;
+	$scope.header_com_logout_pop = false;
+	$scope.header_username = $cookieStore.get("iotcloud-token").loginName;
+
+	//user logout
+	$scope.logout = function(){
+		$scope.header_com_mask = true;
+		$scope.header_com_logout_pop = true;
+	};
+
+	$scope.logout_ok = function(){
+        $http.get(baseUrl.getUrl()+"/api/1/user/logout/").success(function(data){
+            if(data.code=="200"){
+                // alert("登出成功");
+
+                window.location.href = "/login.html"
+            }
+        });
+    };
+
+    $scope.logout_cancel = function(){
+		$scope.header_com_mask = false;
+		$scope.header_com_logout_pop = false;
+	};
 
 })
