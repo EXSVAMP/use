9d7ed8bce91b636fe9a5e9ac5cf4a97327fd91b1
huang <huangwei@exingcai.com>
2016-09-22 11:04:49 +0800

    camera dev

---
+++ modules/pages/camera/camera.sync.js
@@ -0,0 +1,26 @@
+var app = angular.module('RDash');
+app.register.controller("cameraCtrl", function ($scope, $http, $location, $uibModal, $cookieStore, baseUrl, $rootScope) {
+    console.log("5678sss");
+    var urlBase=baseUrl.getUrl();
+
+$scope.test = [
+      {
+        id:2,
+        name: 'a2'
+      },
+      {
+        id:4,
+        name: 'a4'
+      },
+
+    {
+        id:3,
+        name: 'a3'
+    }
+    ]
+
+
+
+
+
+})
--- modules/router/router.js
+++ modules/router/router.js
@@ -4,7 +4,7 @@
 var card = require("pages/card/card");
 var content = require("pages/content/content");
 // var eventlog = require("pages/eventlog/eventlog");
-// var camera = require("pages/camera/camera");
+var camera = require("pages/camera/camera");
 // var reader = require("pages/reader/reader");
 // var location = require("pages/location/location");
 // var imgs = require("pages/imgs/imgs");
@@ -21,10 +21,11 @@ app.config(function ($stateProvider, $urlRouterProvider,$controllerProvider) {
     // Application routes
     $stateProvider
         .state('card', card)
-        .state('content', content);
+        .state('content', content)
+        .state('camera', camera);
         // .state('eventlog', eventlog)
         // .state('reader', reader)
-        // .state('camera', camera)
+
         // .state('location', location)
 
         // .state('imgs', imgs)
