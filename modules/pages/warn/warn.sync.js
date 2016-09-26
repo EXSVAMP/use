476191f9aeab16d79ee8c1216b1fac9d31651dbd
dongwei <dongwei@1b2b.cn>
2016-09-26 17:34:07 +0800

    警报列表

--- modules/pages/warn/warn.sync.js
+++ modules/pages/warn/warn.sync.js
@@ -3,13 +3,10 @@ app.register.controller("warnCtrl", function ($scope, $http, $timeout,ListServic
     ListService.init($scope,'/api/1/eventlog/');
     $scope.params.event_feedback_type=1;
     $scope.refresh();
-    $scope.selections={event_type:[{name:'入库',value:'0'},{name:'移动',value:'1'},{name:'消失',value:'2'},{name:'出库',value:'3'},{name:'非正常',value:'4'}]}
-    $scope.onSelect=function(item,key){
-        console.log(item.value);
-        // utils.set($scope,key,item.value);
-        // console.log($scope.params.event_type)
-    }
-
+    $scope.selections={};
+    $scope.selections.event_type=[{name:'入库',value:'0'},{name:'移动',value:'1'},{name:'消失',value:'2'},{name:'出库',value:'3'},{name:'非正常',value:'4'}];
+    $scope.selections.event_feedback=[{name:'通过',value:'0'},{name:'报警',value:'1'},{name:'通知',value:'2'}];
+    $scope.selections.handle_result=[{name:'未处理',value:'0'},{name:'保持',value:'1'},{name:'解除',value:'2'}];
     $timeout(function(){
         $('.date-picker').datepicker({
             language: 'zh',
