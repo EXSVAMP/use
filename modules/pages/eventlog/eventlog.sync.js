ea3892a3b6492c503a78e040d2d4784a7d9f5963
huang <huangwei@exingcai.com>
2016-09-28 19:05:13 +0800

    eventlog

--- modules/pages/eventlog/eventlog.sync.js
+++ modules/pages/eventlog/eventlog.sync.js
@@ -1,4 +1,31 @@
 var app = angular.module('RDash');
-app.register.controller("eventlogCtrl", function ($scope, $http, $timeout,$location,listService,params) {
+app.register.controller("eventlogCtrl", function ($scope, $http, $timeout,$location,baseUrl,listService,params) {
+    $scope.numbers = [10,20,30,40,50];
+    $scope.selections={};
+    $scope.selections.event_type={"-1":'--------'};
+    $scope.selections.event_feedback_type={"-1":'--------'};
+    $scope.selections.handle_result={"-1":'--------'};
+    $http.get(baseUrl.getUrl()+'/api/1/common/choices/?key=eventlog').success(function(data){
+        if(data.code==200){
+            angular.merge($scope.selections,data.data);
+
+        }
+    });
+
+    console.log($scope.selections)
+    
+    
+    
+    
+
+
+
+
+
+    listService.init($scope,'/api/1/eventlog/');
+
+
+
+
 
 });
