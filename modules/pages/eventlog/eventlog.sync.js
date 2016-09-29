cb69fd4944fa69ab88a0229b7f2840495691fe7b
huang <huangwei@exingcai.com>
2016-09-29 17:19:01 +0800

    eventlog

--- modules/pages/eventlog/eventlog.sync.js
+++ modules/pages/eventlog/eventlog.sync.js
@@ -1,28 +1,63 @@
 var app = angular.module('RDash');
-app.register.controller("eventlogCtrl", function ($scope, $http, $timeout,$location,baseUrl,listService,params) {
-    $scope.numbers = [10,20,30,40,50];
+app.register.controller("eventlogCtrl", function ($scope, $http, $timeout,$location,baseUrl,listService,global) {
+
     $scope.selections={};
+    $scope.camera=[];
+    $scope.reader=[];
     $scope.selections.event_type={"-1":'--------'};
     $scope.selections.event_feedback_type={"-1":'--------'};
     $scope.selections.handle_result={"-1":'--------'};
+    $scope.selections.numbers=global.pageNumSelections;
+    listService.init($scope,'/api/1/eventlog/');
+    $scope.refresh();
+    $scope.tabSwitch = 0;
+    $scope.detail={};
+    // $scope.active=true;
+    $scope.detail.img2='http://img0.imgtn.bdimg.com/it/u=3761389663,2619900045&fm=11&gp=0.jpg';
     $http.get(baseUrl.getUrl()+'/api/1/common/choices/?key=eventlog').success(function(data){
         if(data.code==200){
             angular.merge($scope.selections,data.data);
 
         }
     });
+    $http.get(baseUrl.getUrl()+"/api/1/camera/").success(function(data){
+        if(data.code==200){
+          $scope.camera=data.data;
+            $scope.camera.push({id: '-1', serial_number: '-------------'});
+
+        }
+    });
+    $http.get(baseUrl.getUrl()+"/api/1/reader/").success(function(data){
+        if(data.code==200){
+            $scope.reader=data.data;
+            $scope.reader.push({id: '-1', serial_number: '-------------'});
+
+        }
+    });
 
     console.log($scope.selections)
-    
-    
-    
-    
 
 
 
+    $scope.showDetail=function(id){
+        $http.get(baseUrl.getUrl()+'/api/1/eventlog/'+id+"/").success(function(data){
+            if(data.code==200){
+                $scope.detail = data.data;
+                $scope.detail.img2='http://img0.imgtn.bdimg.com/it/u=3761389663,2619900045&fm=11&gp=0.jpg';
+                $scope.detail.records=[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}]
+                var rfid_list_display = '';
+                angular.forEach($scope.detail.rfid_list,function(item){
+                    if(rfid_list_display!=''){
+                        rfid_list_display+=',';
+                    }
+                    rfid_list_display+=item.rfid_id;
+                });
+                $scope.detail.rfid_list_display=rfid_list_display;
+            }
+        });
+    }
 
 
-    listService.init($scope,'/api/1/eventlog/');
 
 
 
