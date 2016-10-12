2c907f7c63225ad9e48698cb7e0df8b092567645
huang <huangwei@exingcai.com>
2016-10-12 16:34:10 +0800

    function

--- modules/pages/functionSet/functionSet.sync.js
+++ modules/pages/functionSet/functionSet.sync.js
@@ -1,4 +1,9 @@
 var app = angular.module('RDash');
 app.register.controller("functionSetCtrl", function ($scope, $http, $timeout,$location,baseUrl,listService,global,utils,popService) {
-
+    $scope.item={};
+    $scope.item.checked=false;
+     $scope.change=function(){
+         alert("123");
+     }
+    $scope.selections=['警报1','警报2','警报3','警报4'];
 });
