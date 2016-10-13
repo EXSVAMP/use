3a3f89e597ea43d7bcaa04bde31a9d72eea9b77e
jiangyuanyuan <jiangyuanyuan@1b2b.cn>
2016-10-13 14:29:30 +0800

    statistics

--- modules/pages/statistics/statistics.sync.js
+++ modules/pages/statistics/statistics.sync.js
@@ -59,6 +59,10 @@ app.register.controller("statisticsCtrl", function ($scope, $http, $location, $u
       		if(data.code == 200){
       			var dataTemp = data.data;
       			$scope.rfid_total = dataTemp.total;
+      			$scope.rfid_total_0 = $scope.rfid_total;
+      			if($scope.rfid_total == 0){
+  					$scope.rfid_total_0 = 1;
+  				}
   				$scope.rfid_broken = dataTemp.broken;
   				$scope.rfid_unuse = dataTemp.unuse;
   				$scope.rfid_replaced = dataTemp.replaced;
@@ -115,6 +119,10 @@ app.register.controller("statisticsCtrl", function ($scope, $http, $location, $u
       		if(data.code == 200){
       			var dataTemp = data.data;
       			$scope.rfidcont_total = dataTemp.total;
+      			$scope.rfidcont_total_0 = $scope.rfidcont_total;
+      			if($scope.rfidcont_total == 0){
+  					$scope.rfidcont_total_0 = 1;
+  				}
   				$scope.rfidcont_finish_out_repertory = dataTemp.finish_out_repertory;
   				$scope.rfidcont_out_repertory = dataTemp.out_repertory;
   				$scope.rfidcont_in_monitor = dataTemp.in_monitor;
@@ -137,6 +145,10 @@ app.register.controller("statisticsCtrl", function ($scope, $http, $location, $u
       		if(data.code == 200){
       			var dataTemp = data.data;
       			$scope.rfidalarm_total = dataTemp.total;
+      			$scope.rfidalarm_total_0 = $scope.rfidalarm_total;
+      			if($scope.rfidalarm_total == 0){
+  					$scope.rfidalarm_total_0 = 1;
+  				}
   				$scope.rfidalarm_yesterday = dataTemp.yesterday;
   				$scope.rfidalarm_last_week = dataTemp.last_week;
   				$scope.rfidalarm_last_month = dataTemp.last_month; 				
@@ -157,8 +169,12 @@ app.register.controller("statisticsCtrl", function ($scope, $http, $location, $u
       			$scope.rfidcamera_total = dataTemp.total;
 
       			var outin_storage = dataTemp.outin_storage;
-
       			$scope.outin_storage_total = outin_storage.total;
+      			$scope.outin_storage_total_0 = $scope.outin_storage_total;
+      			if($scope.outin_storage_total == 0){
+  					$scope.outin_storage_total_0 = 1;
+  				}
+
       			$scope.outin_storage_deleted = outin_storage.deleted;
       			var outin_storage_deleted_info = $scope.toDecimal(outin_storage.deleted,outin_storage.total);
   				$scope.outin_storage_deleted_ratio = outin_storage_deleted_info.s2;
@@ -172,11 +188,15 @@ app.register.controller("statisticsCtrl", function ($scope, $http, $location, $u
   				$scope.outin_storage_unuse = outin_storage.unuse;
   				var outin_storage_unuse_info = $scope.toDecimal(outin_storage.unuse,outin_storage.total);
   				$scope.outin_storage_unuse_ratio = outin_storage_unuse_info.s2;
-  				$scope.outin_storage_unuse_half = outin_storage_unuse_info.isHalf;		
+  				$scope.outin_storage_unuse_half = outin_storage_unuse_info.isHalf;	
 
   				var monitor = dataTemp.monitor;
-
       			$scope.monitor_total = monitor.total;
+      			$scope.monitor_total_0 = $scope.monitor_total;
+      			if($scope.monitor_total == 0){
+  					$scope.monitor_total_0 = 1;
+  				}
+
       			$scope.monitor_deleted = monitor.deleted;
       			var monitor_deleted_info = $scope.toDecimal(monitor.deleted,monitor.total);
   				$scope.monitor_deleted_ratio = monitor_deleted_info.s2;
@@ -190,17 +210,19 @@ app.register.controller("statisticsCtrl", function ($scope, $http, $location, $u
   				$scope.monitor_unuse = monitor.unuse;
   				var monitor_unuse_info = $scope.toDecimal(monitor.unuse,monitor.total);
   				$scope.monitor_unuse_ratio = monitor_unuse_info.s2;
-  				$scope.monitor_unuse_half = monitor_unuse_info.isHalf;			
+  				$scope.monitor_unuse_half = monitor_unuse_info.isHalf;		
 
   				var zoom = dataTemp.zoom;
-
       			$scope.zoom_total = zoom.total;
-      			//$scope.zoom_total_prg = zoom.total;
+      			$scope.zoom_total_0 = $scope.zoom_total;
+      			if($scope.zoom_total == 0){
+  					$scope.zoom_total_0 = 1;
+  				}
+
       			$scope.zoom_deleted = zoom.deleted;
       			var zoom_deleted_info = $scope.toDecimal(zoom.deleted,zoom.total);
   				$scope.zoom_deleted_ratio = zoom_deleted_info.s2;
   				$scope.zoom_deleted_half = zoom_deleted_info.isHalf;
-  				//if($scope.zoom_deleted == 0 && $scope.zoom_total_prg)
 
   				$scope.zoom_using = zoom.using;
   				var zoom_using_info = $scope.toDecimal(zoom.using,zoom.total);
@@ -211,6 +233,7 @@ app.register.controller("statisticsCtrl", function ($scope, $http, $location, $u
   				var zoom_unuse_info = $scope.toDecimal(zoom.unuse,zoom.total);
   				$scope.zoom_unuse_ratio = zoom_unuse_info.s2;
   				$scope.zoom_unuse_half = zoom_unuse_info.isHalf;
+  				
       		}
     	}).error(function(data,state){
       	if(state == 403){
@@ -220,5 +243,87 @@ app.register.controller("statisticsCtrl", function ($scope, $http, $location, $u
   }
 
   $scope.getRFIDCamera();
+
+  $scope.getRFIDReader = function(){
+  		$http.get(baseUrl.getUrl() + "/api/2/count/rfidreader").success(function(data){
+      		if(data.code == 200){
+      			var dataTemp = data.data;
+      			$scope.rfidreader_total = dataTemp.total;
+
+      			var r_outin_storage = dataTemp.outin_storage;
+      			$scope.r_outin_storage_total = r_outin_storage.total;
+      			$scope.r_outin_storage_total_0 = $scope.r_outin_storage_total;
+      			if($scope.r_outin_storage_total == 0){
+  					$scope.r_outin_storage_total_0 = 1;
+  				}
+
+      			$scope.r_outin_storage_deleted = r_outin_storage.deleted;
+      			var r_outin_storage_deleted_info = $scope.toDecimal(r_outin_storage.deleted,r_outin_storage.total);
+  				$scope.r_outin_storage_deleted_ratio = r_outin_storage_deleted_info.s2;
+  				$scope.r_outin_storage_deleted_half = r_outin_storage_deleted_info.isHalf;
+
+  				$scope.r_outin_storage_using = r_outin_storage.using;
+  				var r_outin_storage_using_info = $scope.toDecimal(r_outin_storage.using,r_outin_storage.total);
+  				$scope.r_outin_storage_using_ratio = r_outin_storage_using_info.s2;
+  				$scope.r_outin_storage_using_half = r_outin_storage_using_info.isHalf;
+
+  				$scope.r_outin_storage_unuse = r_outin_storage.unuse;
+  				var r_outin_storage_unuse_info = $scope.toDecimal(r_outin_storage.unuse,r_outin_storage.total);
+  				$scope.r_outin_storage_unuse_ratio = r_outin_storage_unuse_info.s2;
+  				$scope.r_outin_storage_unuse_half = r_outin_storage_unuse_info.isHalf;	
+
+  				var r_monitor = dataTemp.monitor;
+      			$scope.r_monitor_total = r_monitor.total;
+      			$scope.r_monitor_total_0 = $scope.r_monitor_total;
+      			if($scope.r_monitor_total == 0){
+  					$scope.r_monitor_total_0 = 1;
+  				}
+
+      			$scope.r_monitor_deleted = r_monitor.deleted;
+      			var r_monitor_deleted_info = $scope.toDecimal(r_monitor.deleted,r_monitor.total);
+  				$scope.r_monitor_deleted_ratio = r_monitor_deleted_info.s2;
+  				$scope.r_monitor_deleted_half = r_monitor_deleted_info.isHalf;
+
+  				$scope.r_monitor_using = r_monitor.using;
+  				var r_monitor_using_info = $scope.toDecimal(r_monitor.using,r_monitor.total);
+  				$scope.r_monitor_using_ratio = r_monitor_using_info.s2;
+  				$scope.r_monitor_using_half = r_monitor_using_info.isHalf;
+
+  				$scope.r_monitor_unuse = r_monitor.unuse;
+  				var r_monitor_unuse_info = $scope.toDecimal(r_monitor.unuse,r_monitor.total);
+  				$scope.r_monitor_unuse_ratio = r_monitor_unuse_info.s2;
+  				$scope.r_monitor_unuse_half = r_monitor_unuse_info.isHalf;		
+
+  				var r_handheld_scan = dataTemp.handheld_scan;
+      			$scope.r_handheld_scan_total = r_handheld_scan.total;
+      			$scope.r_handheld_scan_total_0 = $scope.r_handheld_scan_total;
+      			if($scope.r_handheld_scan_total == 0){
+  					$scope.r_handheld_scan_total_0 = 1;
+  				}
+
+      			$scope.r_handheld_scan_deleted = r_handheld_scan.deleted;
+      			var r_handheld_scan_deleted_info = $scope.toDecimal(r_handheld_scan.deleted,r_handheld_scan.total);
+  				$scope.r_handheld_scan_deleted_ratio = r_handheld_scan_deleted_info.s2;
+  				$scope.r_handheld_scan_deleted_half = r_handheld_scan_deleted_info.isHalf;
+
+  				$scope.r_handheld_scan_using = r_handheld_scan.using;
+  				var r_handheld_scan_using_info = $scope.toDecimal(r_handheld_scan.using,r_handheld_scan.total);
+  				$scope.r_handheld_scan_using_ratio = r_handheld_scan_using_info.s2;
+  				$scope.r_handheld_scan_using_half = r_handheld_scan_using_info.isHalf;
+
+  				$scope.r_handheld_scan_unuse = r_handheld_scan.unuse;
+  				var r_handheld_scan_unuse_info = $scope.toDecimal(r_handheld_scan.unuse,r_handheld_scan.total);
+  				$scope.r_handheld_scan_unuse_ratio = r_handheld_scan_unuse_info.s2;
+  				$scope.r_handheld_scan_unuse_half = r_handheld_scan_unuse_info.isHalf;
+  				
+      		}
+    	}).error(function(data,state){
+      	if(state == 403){
+        	baseUrl.redirect()
+      	}
+    });
+  }
+
+  $scope.getRFIDReader();
 
 })
