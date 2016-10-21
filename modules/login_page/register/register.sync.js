4bab07f0e365a3477ca1adca7a65aa37da1e97a2
jiangyuanyuan <jiangyuanyuan@1b2b.cn>
2016-10-21 11:23:25 +0800

    code test

--- modules/login_page/register/register.sync.js
+++ modules/login_page/register/register.sync.js
@@ -28,38 +28,39 @@ app.register.controller("registerCtrl", function ($scope, $http, $location, $tim
     $scope.goNext = function(iNext){
         if(iNext == 2){
             var username = $scope.information.username;
-            console.log($scope.information.password);
+            //console.log($scope.information.password);
             var userpass = $scope.information.password;
             var userre_pass = $scope.information.re_password;
-            console.log(username+","+userpass+","+userre_pass);
+            //console.log(username+","+userpass+","+userre_pass);
             if(username && userpass && userre_pass){
-                console.log(123);
+               //console.log(123);
                 if(username.length>20){
-                    ngDialog.open({
-                        template: '<p style=\"text-align: center\">用户名最长20位</p>',
-                        plain: true
-                    });
+                    // ngDialog.open({
+                    //     template: '<p style=\"text-align: center\">用户名最长20位</p>',
+                    //     plain: true
+                    // });
                 }else if(userpass.length<6 || userpass.length>20){
-                    ngDialog.open({
-                        template: '<p style=\"text-align: center\">密码设为6-20位</p>',
-                        plain: true
-                    });
+                    // ngDialog.open({
+                    //     template: '<p style=\"text-align: center\">密码设为6-20位</p>',
+                    //     plain: true
+                    // });
                 }else if(userre_pass != userpass){
-                    ngDialog.open({
-                        template: '<p style=\"text-align: center\">确认密码与密码不一致</p>',
-                        plain: true
-                    });
+                    // ngDialog.open({
+                    //     template: '<p style=\"text-align: center\">确认密码与密码不一致</p>',
+                    //     plain: true
+                    // });
                 }else{
                     $scope.register_success = 2;
                     $scope.regstep2 = "reg-step-i reg-step-i-active";
                     $scope.regarrow2 = "/statics/lib/img/icon_arrow_32_32_01 copy.png";
                 }
                 
-            }else
-                ngDialog.open({
-                    template: '<p style=\"text-align: center\">用户名，密码，重复密码为必填项</p>',
-                    plain: true
-                });
+            }
+            // else
+            //     ngDialog.open({
+            //         template: '<p style=\"text-align: center\">用户名，密码，重复密码为必填项</p>',
+            //         plain: true
+            //     });
         }else if(iNext == 3){
             var name = $scope.information.name;
             var address = $scope.information.address;
@@ -71,18 +72,20 @@ app.register.controller("registerCtrl", function ($scope, $http, $location, $tim
                 $scope.regstep3 = "reg-step-i reg-step-i-active";
                 if(data.code=="200"){
                     $scope.register_res = "恭喜你，注册成功!";
-                    scope.countdown1 = 5;
+                    $scope.register_res_fail = "";
+                    $scope.countdown1 = 5;
                     $scope.register_ok = true;
                     $scope.register_fail = false;
                     var toDo = function () {
                         $scope.countdown1--;
-                        if($scope.countdown1 == 0){
+                        if($scope.countdown1 == 1){
                             window.location.href = "/login";
                         }
                     };
                     $interval(toDo, 1000, 5);
                 }else{
-                    $scope.register_res = "抱歉，注册失败!<br/>"+data.message;
+                    $scope.register_res = data.message+"，注册失败!";
+                    $scope.register_res_fail = "register_res_fail";
                     $scope.countdown2 = 5;
                     $scope.register_fail = true;
                     $scope.register_ok = false;
@@ -98,7 +101,8 @@ app.register.controller("registerCtrl", function ($scope, $http, $location, $tim
                     $interval(toDo, 1000, 5);
                 }
             }).error(function(data){
-                $scope.register_res = "抱歉，注册失败!<br/>"+data.message;
+                $scope.register_res = data.message+"，注册失败!";
+                $scope.register_res_fail = "register_res_fail";
                 $scope.countdown2 = 5;
                 $scope.register_fail = true;
                 $scope.register_ok = false;
@@ -113,8 +117,9 @@ app.register.controller("registerCtrl", function ($scope, $http, $location, $tim
                 };
                 $interval(toDo, 1000, 5);
             })
-            }else
-                alert("信息填写不全！");
+             }
+            //else
+            //     alert("信息填写不全！");
         }
     }
 
