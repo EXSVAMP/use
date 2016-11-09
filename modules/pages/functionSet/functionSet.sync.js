e075a63039458db6b2bea6d88fca058badb64196
huang <huangwei@exingcai.com>
2016-11-09 18:46:57 +0800

    functionset modify

--- modules/pages/functionSet/functionSet.sync.js
+++ modules/pages/functionSet/functionSet.sync.js
@@ -8,24 +8,43 @@ app.register.controller("functionSetCtrl", function ($scope, $http, $timeout,$lo
          alert("123");
      }
     $scope.selections=['警报1','警报2','警报3','警报4'];
+    $scope.search_common=function(){
+        $http.get(urlBase+"/api/2/func/mainset").success(function(data){
+            $scope.dataList_common=data.data;
+            angular.forEach($scope.dataList_common,function(item){
+                if(item.status==0){
+                    item.checked=true;
+                }
+                if(item.status==1){
+                    item.checked=false;
+                }
+            })
 
-    $http.get(urlBase+"/api/2/func/mainset").success(function(data){
-         $scope.dataList_common=data.data;
-        angular.forEach($scope.dataList_common,function(item){
-            if(item.status==0){
-               item.checked=true;
+        }).error(function (data, state) {
+            if (state == 403) {
+                baseUrl.redirect()
             }
-            if(item.status==1){
-                item.checked=false;
-            }
-        })
+        });
+    }
 
-    }).error(function (data, state) {
-        if (state == 403) {
-            baseUrl.redirect()
+
+
+    $scope.setCommon=function(main_event,state){
+        $scope.main_event=main_event;
+        if(state==false){
+            $scope.status="1";
         }
-    });
-
-
+        if(state==true){
+            $scope.status="0";
+        }
+       $http.post(urlBase+"/api/2/func/mainset",{"main_event":$scope.main_event,"status":$scope.status}).success(function(data){
+           $scope.search_common();
+       }).error(function (data, state) {
+           if (state == 403) {
+               baseUrl.redirect()
+           }
+       });
+    }
+    $scope.search_common();
 
 });
