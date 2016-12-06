ae5bab4b96bf23bbb2fc216780769bbc63b810f6
huang <huangwei@exingcai.com>
2016-12-06 18:25:57 +0800

    auto test modify

--- modules/common/constant.js
+++ modules/common/constant.js
@@ -1,6 +1,7 @@
 var app = angular.module('RDash');
 app.constant("constant",{
     // url:"http://139.196.148.70:9011",
+    // websocket_url:"139.196.148.70",
     url:"http://172.16.83.149:9011",
     websocket_url:"172.16.83.149",
     websocket_userName:"iotclould",
